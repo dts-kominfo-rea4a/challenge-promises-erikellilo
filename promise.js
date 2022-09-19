37f18d95c3fabda47bafb395f28f094806d5c055
erikellilo <erikellilo@gmail.com>
2022-09-19 19:54:24 +0700

    inital commit

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async emosi => {
+	try {
+		let count = [];
+		const theaterIXX = await promiseTheaterIXX();
+		const theaterVGC = await promiseTheaterVGC();
+		if (!emosi) return;
+		theaterIXX.forEach(({ hasil }) => emosi === hasil && count.push(hasil));
+		theaterVGC.forEach(({ hasil }) => emosi === hasil && count.push(hasil));
+		return "" + count.length;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 module.exports = {
-  promiseOutput,
+	promiseOutput,
 };
